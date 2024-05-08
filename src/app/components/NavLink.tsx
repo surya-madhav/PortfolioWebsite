97f830ced5cba495f6f3e4e543e9c6b9cee4342a
Sai Surya Madhav <rebbapragada.s@northeastern.edu>
2024-05-07 22:09:26 -0400

    errors fixed

--- src/app/components/NavLink.tsx
+++ src/app/components/NavLink.tsx
@@ -2,7 +2,7 @@ import Link from "next/link"
 import React from "react"
 
 
-const NavLink = ({href, title}) =>{
+const NavLink = ({href, title}: {href: string, title: string}) =>{
     return (<Link href={href} className="block py-2 pl-3 pr-4 text-gray-200 hover:text-orange-300">{title}</Link>)
 }
 export default NavLink;
--- src/app/scripts/analytics.js
+++
@@ -1,8 +0,0 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-SS9V77ZVSX"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-SS9V77ZVSX');
-</script>
