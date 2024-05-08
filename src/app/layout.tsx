ae9b66978511c5afb50c94c0d0cde6444a2283f6
Sai Surya Madhav <rebbapragada.s@northeastern.edu>
2024-05-07 22:03:25 -0400

    gcp and packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
 const inter = Inter({ subsets: ["latin"] });
+import { GoogleAnalytics } from "nextjs-google-analytics";
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,6 +19,7 @@ export default function RootLayout({
 }>) {
   return ( 
     <html lang="en">
+      <GoogleAnalytics />
       <body className={inter.className}>{children}</body>
     </html>
   );
---
+++ src/app/scripts/analytics.js
@@ -0,0 +1,8 @@
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-SS9V77ZVSX"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-SS9V77ZVSX');
+</script>
